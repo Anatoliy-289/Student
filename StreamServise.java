7ac589349d7f2eba49624a16348beed13c8ebf41
Anatoliy Turanskiy <anatol911@yandex.ru>
2024-08-30 20:48:10 +0500

    Создан класс StreamServise

---
+++ StreamServise.java
@@ -0,0 +1,13 @@
+public class StreamServise {
+    
+    public static void SortStream (List<StudentStream> stream) {
+        StreamComparator streamComparator = new StreamComparator() {
+            studentStream.sort(StudentStream);
+        }
+        
+    }
+
+}
+
+
+
